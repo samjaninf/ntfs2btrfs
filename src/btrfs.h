e981a48c6c9acbfc7f7850a576ab902f92cc0d84
Mark Harmstone <mark@harmstone.com>
2026-07-05 15:19:47 +0100

    replace BTRFS_MAGIC

--- src/btrfs.h
+++ src/btrfs.h
@@ -17,7 +17,8 @@ using le16 = uint16_t;
 
 static const uint64_t superblock_addrs[] = { 0x10000, 0x4000000, 0x4000000000, 0x4000000000000, 0 };
 
-#define BTRFS_MAGIC         0x4d5f53665248425f
+constexpr uint64_t BTRFS_MAGIC = 0x4d5f53665248425f;
+
 #define SUBVOL_ROOT_INODE   0x100
 
 enum class btrfs_key_type : uint8_t {
